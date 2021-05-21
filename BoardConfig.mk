05348d8f6916bce6f3d200ebd2d0740ea55c4129
nao04 <korewanao@outlook.com>
2021-05-21 14:49:37 +0000

    RMX1971: Decommonize kernel rules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,8 +13,23 @@ DEVICE_PATH := device/realme/RMX1971
 TARGET_OTA_ASSERT_DEVICE := RMX1971,RMX1971CN
 
 # Kernel
-TARGET_KERNEL_CONFIG := RMX1971_defconfig
+BOARD_KERNEL_CMDLINE     := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 swiotlb=1 loop.max_part=7
+BOARD_KERNEL_CMDLINE     += androidboot.selinux=permissive
+BOARD_KERNEL_BASE        := 0x00000000
+BOARD_KERNEL_PAGESIZE    := 4096
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_KERNEL_IMAGE_NAME  := Image.gz-dtb
+BOARD_RAMDISK_OFFSET     := 0x01000000
 
+TARGET_KERNEL_ARCH 	 := arm64
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_SOURCE := kernel/msm-4.9
+TARGET_KERNEL_CONFIG := RMX1971_defconfig
+endif
+
+# Partitions
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
 # Verified Boot
